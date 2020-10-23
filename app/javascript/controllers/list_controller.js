74230633cdc982e5534d640066d73af1a527a4a9
Rock Hymas <rock@hymasfamily.org>
2020-10-23 08:00:46 -0600

    Cleanup stimulus list controller

--- app/javascript/controllers/list_controller.js
+++ app/javascript/controllers/list_controller.js
@@ -2,13 +2,13 @@ import { Controller } from 'stimulus';
 import consumer from "channels/consumer"
 
 export default class extends Controller {
+  static targets = ["template"];
+
   connect() {
     console.log("hello from StimulusJS")
     var element = this.element;
     var list_id = element.dataset.listId,
-    todoTemplate = document.querySelector('[data-role="todo-template"]');
-
-    // $element.animate({ scrollTop: $element.prop("scrollHeight")}, 1000)        
+    todoTemplate = this.templateTarget;
 
     consumer.subscriptions.create(
       {
@@ -20,7 +20,6 @@ export default class extends Controller {
           var content = todoTemplate.cloneNode(true);
           content.querySelector('[data-role="todo-title"]').textContent = data.title;
           [...content.children].forEach(function(child) { element.appendChild(child); });
-          // element.animate({ scrollTop: element.prop("scrollHeight")}, 1000);
         }
       }
     );
