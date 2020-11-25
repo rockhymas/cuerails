7ee5dcb8bb551c4c614224c62432a8988f0e735c
Rock Hymas <rock@hymasfamily.org>
2020-11-24 21:01:38 -0700

    Users have lists, list partials

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :lists
+
+  def current_day_plan
+    zone = ActiveSupport::TimeZone.new(time_zone)
+    today = zone.now.to_date
+
+    lists.where("date <= :today", {today: today}).first
+  end
 end
