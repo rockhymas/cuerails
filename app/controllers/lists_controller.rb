a6aa0ec5bf576040c8c17ead6a8396e2364c1596
Rock Hymas <rock@hymasfamily.org>
2020-12-21 18:09:37 -0700

    Cleaning up controllers and views

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,59 +1,9 @@
 class ListsController < ApplicationController
-  before_action :set_list, only: [:show, :edit, :update, :destroy]
+  before_action :set_list, only: [:show, :destroy]
 
-  # GET /lists
-  # GET /lists.json
-  def index
-    @lists = List.all
-  end
-
-  # GET /lists/1
-  # GET /lists/1.json
   def show
   end
 
-  # GET /lists/new
-  def new
-    @list = List.new
-  end
-
-  # GET /lists/1/edit
-  def edit
-  end
-
-  # POST /lists
-  # POST /lists.json
-  def create
-    @list = List.new(list_params)
-    @list.user = current_user
-
-    respond_to do |format|
-      if @list.save
-        format.html { redirect_to @list, notice: 'List was successfully created.' }
-        format.json { render :show, status: :created, location: @list }
-      else
-        format.html { render :new }
-        format.json { render json: @list.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /lists/1
-  # PATCH/PUT /lists/1.json
-  def update
-    respond_to do |format|
-      if @list.update(list_params)
-        format.html { redirect_to @list, notice: 'List was successfully updated.' }
-        format.json { render :show, status: :ok, location: @list }
-      else
-        format.html { render :edit }
-        format.json { render json: @list.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /lists/1
-  # DELETE /lists/1.json
   def destroy
     @list.destroy
     respond_to do |format|
