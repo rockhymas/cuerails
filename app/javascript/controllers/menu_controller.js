e0b35bedb420e211d3211e45f06a2aa7f3332041
Rock Hymas <rock@hymasfamily.org>
2021-01-07 09:17:41 -0700

    More work on sidebar

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -5,8 +5,10 @@ export default class extends ApplicationController {
 
   toggle(e) {
     if (this.dropdownTarget.classList.contains('hidden')) {
+      // TODO: add listener for clicking anywhere else, which will dismiss the menu
       this.dropdownTarget.classList.remove('hidden');
     } else {
+      // TODO: remove listener added above
       this.dropdownTarget.classList.add('hidden');
     }
   }
